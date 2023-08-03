5d17cf4b6df5ade42675fb4933d0fb8df750a451
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-03 13:17:44 +0100

    update

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    lockboxes
+    """
+    visited = []
+
+    def dfs(boxNumber):
+        """
+        lockboxes
+        """
+        visited.append(boxNumber)
+        for key in boxes[boxNumber]:
+            if key not in visited:
+                dfs(key)
+
+    dfs(0)
+    return len(boxes) == len(visited)

8878e6a2ff938af9bf02a0afa9280918ba192fef
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-15 14:03:52 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,16 +5,17 @@ import sys
 
 
 def print_metrics(metrics):
-    """Print metrics"""
-    print("File size: ", metrics["total_size"])
+    """print metrics"""
+    print("File size:", metrics["total_size"])
 
     for status_code in sorted(metrics["status_codes"]):
         if status_code.isdigit():
             print(f"{status_code}: {metrics['status_codes'][status_code]}")
 
 
+
 def parse_line(line):
-    """Parse line"""
+    """parse line"""
     split = line.strip().split()
     if len(split) != 9:
         return None
@@ -22,32 +23,23 @@ def parse_line(line):
     return ip, status_code, int(file_size)
 
 
-def update_metrics(metrics, parsed_line):
-    """Update metrics based on parsed line"""
-    ip, status_code, file_size = parsed_line
-    metrics["total_size"] += file_size
-    metrics["status_codes"][status_code] = metrics["status_codes"].get(status_code, 0) + 1
-
-
-def process_log():
-    """Process log entries and compute metrics"""
+if __name__ == "__main__":
     line_count = 0
     metrics = {
        "total_size": 0,
-       "status_codes": {}
+        "status_codes": {} 
     }
     try:
         for line in sys.stdin:
             parsed_line = parse_line(line)
             if parsed_line:
-                update_metrics(metrics, parsed_line)
+                ip, status_code, file_size = parsed_line
+                metrics["total_size"] += file_size
+                metrics["status_codes"][status_code] = metrics["status_codes"].get(status_code, 0) + 1
                 line_count += 1
                 if line_count == 10:
                     print_metrics(metrics)
                     line_count = 0
     except (KeyboardInterrupt, EOFError):
         print_metrics(metrics)
-
-
-if __name__ == "__main__":
-    process_log()
+        

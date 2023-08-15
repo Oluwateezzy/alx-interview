7a0854fd4d72f56f7b8c77c6e40188fcf4105e0f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-15 13:52:42 +0100

    update

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Log static files"""
+
+import sys
+
+
+def print_metrics(metrics):
+    """print metrics"""
+    print("File size: ", metrics["total_size"])
+
+    for status_code in sorted(metrics["status_codes"]):
+        if status_code.isdigit():
+            print(f"{status_code}: {metrics['status_codes'][status_code]}")
+
+
+
+def parse_line(line):
+    """parse line"""
+    split = line.strip().split()
+    if len(split) != 9:
+        return None
+    ip, _, _, _, _, _, _, status_code, file_size = split
+    return ip, status_code, int(file_size)
+
+
+if __name__ == "__main__":
+    line_count = 0
+    metrics = {
+       "total_size": 0,
+        "status_codes": {} 
+    }
+    try:
+        for line in sys.stdin:
+            parsed_line = parse_line(line)
+            if parsed_line:
+                ip, status_code, file_size = parsed_line
+                metrics["total_size"] += file_size
+                metrics["status_codes"][status_code] = metrics["status_codes"].get(status_code, 0) + 1
+                line_count += 1
+                if line_count == 10:
+                    print_metrics(metrics)
+                    line_count = 0
+    except KeyboardInterrupt:
+        print_metrics(metrics)
+        

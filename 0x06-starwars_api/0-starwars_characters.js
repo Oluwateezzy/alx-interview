42b03bcac59eab8af77035f63db522b8eb01f029
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-05 15:33:13 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,31 @@
 #!/usr/bin/node
 
+/**
+ * print all start wars movive character
+ */
+
 const request = require('request');
 const { argv } = require('process');
 
 const filmUrl = 'https://swapi-api.alx-tools.com/api/films' + '/' + argv[2];
+// make api request
+request(filmUrl, async(err, res, body) => {
+    if (err) {
+        console.error(err);
+    }
+    const characters = JSON.parse(body).characters;
 
-request(filmUrl, async (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-  const characters = JSON.parse(body).characters;
-
-  for (const character of characters) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        if (err) {
-          console.log(err);
-        }
-        const ch = JSON.parse(body).name;
-        console.log(ch);
-        resolve();
-      });
-    });
-  }
+    for (const character of characters) {
+        // wait for each request
+        await new Promise((resolve, reject) => {
+            request(character, (err, res, body) => {
+                if (err) {
+                    console.error(err);
+                }
+                const ch = JSON.parse(body).name;
+                console.log(ch);
+                resolve();
+            });
+        });
+    }
 });

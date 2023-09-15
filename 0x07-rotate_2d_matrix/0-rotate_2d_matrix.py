07110bdf7762502f7c5929579964ec1ac081417e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-15 02:25:31 +0100

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,17 +4,16 @@
 
 def rotate_2d_matrix(matrix):
     """ rotate_2d_matrix(matrix) """
-    
-    l2 = []
-    for i in range(len(matrix)):
-        l2.append(print_column(matrix, i))
-    print(l2)
+
+    clone = matrix.copy()
+    for i in range(len(clone)):
+        matrix[i] = print_column(clone, i)
 
 
 def print_column(matrix, column_index):
     """ print column """
-    l = []
+    lis = []
     for row in matrix:
         if 0 <= column_index < len(row):
-            l.append(row[column_index])
-    return l[::-1]
+            lis.append(row[column_index])
+    return lis[::-1]

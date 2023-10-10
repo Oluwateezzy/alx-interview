f57e6ab13e62e9d7d330947333cd42904eee7a5f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-10-10 16:40:15 +0100

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
 """Maria and Ben are playing a game.
-   Given a set of consecutive integers starting from 1 up to and including n,
-   they take turns choosing a prime number from the set and removing that number 
-   and its multiples from the set. The player that cannot make a move loses the game.
+   Given a set of consecutive integers
+   starting from 1 up to and including n,
+   they take turns choosing a prime number
+   from the set and removing that number
+   and its multiples from the set.
+   The player that cannot make a move loses the game.
 """
+
 
 def isWinner(x, nums):
     """ isWinner """
-    player = {'Ben': 0, 'Maria': 0}
+    player = {"Maria": 0, "Ben": 0}
     n = max(nums)
-    primes = []
-    
-    if n > 2:
-        for i in range(3, n+1):
-            if isPrime(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
     for round in range(x):
-        total = sum((i != 0 and i <= nums[round]) for i in primes[:nums[round] + 1])
-        if (total % 2):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
             winner = "Maria"
         else:
             winner = "Ben"
         if winner:
             player[winner] += 1
-    if player['Maria'] > player['Ben']:
-        return 'Maria'
-    elif player['Ben'] > player['Maria']:
-        return 'Ben'
+    if player["Maria"] > player["Ben"]:
+        return "Maria"
+    elif player["Ben"] > player["Maria"]:
+        return "Ben"
+
     return None
+
+
+def add_prime(n, primes):
+    """ Add prime to list """
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if isPrime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
 
 def isPrime(number):
     """ check prime number """
-    for i in range(2, number + 1):
-        if number % i == 0:
+    for i in range(2, int(number ** 0.5) + 1):
+        if not number % i:
             return False
-        else:
-            return True
+    return True
